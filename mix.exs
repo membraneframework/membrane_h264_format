f282238a225cdc2c7f2ba24310db8ec76b37b2c0
Mateusz Front <mateusz.front@swmansion.com>
2020-06-04 09:43:04 +0200

    bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Caps.Video.H264.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane-caps-video-h264"
 
   def project do
